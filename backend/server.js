7f3ce4808877b558a88c75504bf6dcd0fe0257f9
Robert Lowe <rlowedevelopment@gmail.com>
2025-02-05 12:54:12 -0800

    Updated gitignore file

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import path from 'path';
 
 import apiRoutes from './apiRoutes.js';
 import { connectDB } from './config/db.js';
 import { ENV_VARS } from './config/envVars.js';
 
 const app = express();
+
 const PORT = ENV_VARS.PORT
+const __dirname = path.resolve();
 
 app.use(cors({
     origin: 'http://localhost:5173',
@@ -18,6 +21,14 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/v1", apiRoutes);
+
+if (ENV_VARS.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`);
