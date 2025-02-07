e5c4b0a45cdc2d43c7cc0a1a6f158187fdc35799
Robert Lowe <rlowedevelopment@gmail.com>
2025-02-06 16:34:14 -0800

    Added className prop to footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Footer = () => {
+const Footer = ({ className}) => {
   return (
-    <footer className='py-6 md:px-8 md:py-0 bg-black text-white border-t border-gray-800'>
+    <footer className={`py-6 md:px-8 md:py-0 bg-black text-white border-t border-gray-800 ${className}`}>
         <div className="flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
             <p className="text-balance text-center txtsm leading-loose text-muted-foreground md:text-left">
                 Built by <a href="https://github.com/ChackoTacko" target='_blank' className="font-semibold underline underline-offset-4">Robert Lowe</a>.
